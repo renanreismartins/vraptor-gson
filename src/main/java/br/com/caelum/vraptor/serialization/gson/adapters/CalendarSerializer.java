47ac001b43b2cc431370e7d4e5329acdfd91375e
Renan Reis <renanreismartins@gmail.com>
2012-08-16 18:48:55 -0300

    anotanto adapter com @component para que seja injetado automaticamente

--- src/main/java/br/com/caelum/vraptor/serialization/gson/adapters/CalendarSerializer.java
+++ src/main/java/br/com/caelum/vraptor/serialization/gson/adapters/CalendarSerializer.java
@@ -3,11 +3,14 @@ package br.com.caelum.vraptor.serialization.gson.adapters;
 import java.lang.reflect.Type;
 import java.util.Calendar;
 
+import org.springframework.stereotype.Component;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+@Component
 public class CalendarSerializer implements JsonSerializer<Calendar> {
 
 	@Override
